e1d5953404cbbd9f5294c9b167ec00bc395ac40f
Brian Schnepp <bschneppdev@gmail.com>
2022-06-08 18:46:54 -0600

    Import additional drm code

---
+++ sys/external/gpl2/drm2/dist/drm/v3d/v3d_trace_points.c
@@ -0,0 +1,14 @@
+/*	$NetBSD$	*/
+
+// SPDX-License-Identifier: GPL-2.0+
+/* Copyright (C) 2015 Broadcom */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD$");
+
+#include "v3d_drv.h"
+
+#ifndef __CHECKER__
+#define CREATE_TRACE_POINTS
+#include "v3d_trace.h"
+#endif
---
+++ sys/external/gpl2/drm2/dist/drm/vc4/vc4_trace_points.c
@@ -0,0 +1,16 @@
+/*	$NetBSD$	*/
+
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2015 Broadcom
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD$");
+
+#include "vc4_drv.h"
+
+#ifndef __CHECKER__
+#define CREATE_TRACE_POINTS
+#include "vc4_trace.h"
+#endif
