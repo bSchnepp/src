586a44fa7f1a90f6dc7dde06175c4f54d144f747
Brian Schnepp <bschneppdev@gmail.com>
2022-08-04 00:52:19 -0600

    Revert movement of drm_fb_helper_remove_conflicting_framebuffers back to v3d.

--- sys/external/gpl2/drm2/dist/drm/vc4/vc4_drv.c
+++ sys/external/gpl2/drm2/dist/drm/vc4/vc4_drv.c
@@ -281,20 +281,10 @@ vc4_attach(device_t parent, device_t self, void *aux)
 	drm_mode_config_init(vc4->dev);
 	vc4_gem_init(vc4->dev);
 
-	/* v3d should be the last driver to load, so this is safe. */
-	drm_fb_helper_remove_conflicting_framebuffers(NULL, "vc4drmfb", false);
-
-	error = vc4_kms_load(vc4->dev);
-	if (error < 0)
-		goto unbind_all;
-
 	aprint_naive("\n");
 	aprint_normal(": VC4 Core\n");
 	return;
-
-unbind_all:
-	vc4_gem_destroy(sc->sc_drm_dev);
-	vc4_bo_cache_destroy(sc->sc_drm_dev);
+	
 dev_put:
 	drm_dev_put(sc->sc_drm_dev);
 	return;
